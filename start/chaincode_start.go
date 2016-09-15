f96e21003a973b361740f5bd6060fa1ea71f2e28
mslangford <matt.langford@mckesson.com>
2016-09-14 20:03:44 -0700

    Add files via upload

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -31,10 +31,10 @@ type SimpleChaincode struct {
 // Main
 // ============================================================================================================================
 func main() {
-	err := shim.Start(new(SimpleChaincode))
-	if err != nil {
-		fmt.Printf("Error starting Simple chaincode: %s", err)
-	}
+//	err := shim.Start(new(SimpleChaincode))
+//	if err != nil {
+//		fmt.Printf("Error starting Simple chaincode: %s", err)
+//	}
 }
 
 // Init resets all the things
